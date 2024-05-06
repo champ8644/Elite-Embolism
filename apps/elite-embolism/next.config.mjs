ca0f9bab701ae04e345afa4e68923fb588842328
Champ <varis_champ@hotmail.com>
2024-05-07 03:04:34 +0700

    save

--- apps/elite-embolism/next.config.mjs
+++ apps/elite-embolism/next.config.mjs
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export', // <=== enables static exports
-  reactStrictMode: true,
-}
 
-module.exports = nextConfig
+const isProd = process.env.NODE_ENV === 'production';
+
+export default {
+  reactStrictMode: true,
+  assetPrefix: isProd ? '/Neuro-Resident/' : '',
+  output: 'export', // <=== enables static exports
+};
